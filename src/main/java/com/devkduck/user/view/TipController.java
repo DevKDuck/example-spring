e8740b43a6518db6c77ce8a34f38c90baa5be1a0
DevKDuck <loading95@naver.com>
2024-11-15 19:02:20 +0900

    팁 게시글 상세 /tip/detail/{tipId} 이동

--- src/main/java/com/devkduck/user/view/TipController.java
+++ src/main/java/com/devkduck/user/view/TipController.java
@@ -1,8 +1,9 @@
 package com.devkduck.user.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -16,6 +17,12 @@ public class TipController {
     @GetMapping("/tip/list")
     public String tipBoard(){
         return "/tipBoard";
+    }
+    @GetMapping("/tip/detail/{tipId}")
+    public String tipDetail(@PathVariable("tipId") Long tipId, Model model){
+        System.out.println(tipId);
+        model.addAttribute("tipId", tipId);
+        return "/tipDetail";
     }
 
 }
